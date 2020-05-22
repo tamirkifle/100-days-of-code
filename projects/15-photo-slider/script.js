e30e428cce6b801267e8bd78eb79347da2303110
Tamir K. Yirga <tkifle@gmail.com>
2020-05-22 17:02:38 +0300

    rewrote functions fixed animations

--- projects/15-photo-slider/script.js
+++ projects/15-photo-slider/script.js
@@ -2,81 +2,70 @@ const leftArrow = document.querySelector(".arrow.left");
 const rightArrow = document.querySelector(".arrow.right");
 
 const images = document.getElementsByTagName("img");
-// console.log(images[0]);
 
 
 
-function slideImage(direction){
+function startSlideShow(images){
+    Array.from(images).some((image,index) =>  
+        {
+            if(image.classList.contains("current")){
+                images[indexStep(-1,index,images)].classList.add("to-left");
+                images[indexStep(1,index,images)].classList.add("to-right");
+                return true;
+            }
+            else{
+                images[0].classList.add("on-top","current");
+                images[images.length-1].classList.add("on-top","to-left");
+                images[1].classList.add("on-top","to-right");
+                return true;
+            }
+        });
+}
 
-    if(direction === "left"){
-        document.querySelector(".to-right").classList = "";
+startSlideShow(images);
 
-        document.querySelector(".current").classList.add("to-right");
-        document.querySelector(".current").classList.remove("current");
+function clearClasses(collection){
+    Array.from(collection).forEach((item,index) =>  item.classList = "" );
+}
 
-        document.querySelector(".to-left").classList.add("current");
-        document.querySelector(".to-left").classList.remove("to-left");
+function indexStep(step, index, collection){
+    if(index+step < 0)
+        return collection.length+(index+step);
+    else if(index+step > collection.length-1)
+        return (index+step)- collection.length;
+    else 
+        return index+step;
+}
 
-        let i = (function() {
-            let previousIndex = images.length-1;
-            for (let index=0; index<images.length; index++){
-                    if(images[index].classList.contains("current")){
-                        if(index!==0)
-                            previousIndex = index-1;
-                        return previousIndex;
-                    }
-        }})();
-        images[i].classList.add("on-top");
-        images[i].classList.add("to-left");
-
-    }
-    if(direction === "right"){
-        document.querySelector(".to-left").classList = "";
-
-        document.querySelector(".current").classList.add("to-left");
-        document.querySelector(".current").classList.remove("current");
-
-        document.querySelector(".to-right").classList.add("current");
-        document.querySelector(".to-right").classList.remove("to-right");
-
-        let j = (function() {
-            let nextIndex = 0;
-            for (let index=images.length-1; index>0; index--){
-                    if(images[index].classList.contains("current")){
-                        if(index!==images.length-1)
-                            nextIndex = index+1;
-                        return nextIndex;
-                    }
-        }})();
-        images[j].classList.add("on-top");
-        images[j].classList.add("to-right");
-    }
-    // for (let index=0; index<images.length; index++){
-    //     if(images[index].className === "current"){
-    //         currentIndex = index;
-            
-    //     }
-    //     images[index].removeAttribute("class");
-
-    // }
-    // if(num > 0) {
-    //     images[currentIndex].className = "to-left";
-    // }
-    // else if(num < 0) {
-    //     images[currentIndex].className = "to-right";
-    // }
-    //     if(currentIndex+num >= images.length){
-    //         images[0].className = "current";
-    //     }
-    //     else if(currentIndex+num < 0){
-    //         images[images.length-1].className = "current";
-    //     }
-    //     else {
-    //     images[currentIndex+num].className = "current";
-    //     }
-    //     return;
-    
+function left(){
+    Array.from(images).some((image,index) =>  
+        {
+            if(image.classList.contains("current")){
+                clearClasses(images);
+                images[index].classList.add("on-top","to-right");
+                images[indexStep(-1,index,images)].classList.add("on-top","current");
+                images[indexStep(-2, index,images)].classList.add("to-left");
+                return true;
+            }
+        }
+    );
 
 }
-leftArrow.addEventListener("click",function() {slideImage("left");});
-rightArrow.addEventListener("click",function() {slideImage("right");});
+
+function right(){
+
+    Array.from(images).some((image,index) =>  
+        {
+            if(image.classList.contains("current")){
+                clearClasses(images);
+                images[index].classList.add("on-top","to-left");
+                images[indexStep(1,index,images)].classList.add("on-top","current");
+                images[indexStep(2, index,images)].classList.add("to-right");
+                return true;
+            }
+        }
+    );
+
+}
+leftArrow.addEventListener("click",left);
+rightArrow.addEventListener("click",right);
