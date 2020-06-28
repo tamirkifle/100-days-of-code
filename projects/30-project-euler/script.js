9361a106a9beb630719da1eb5299d651f3ec6196
Tamir K. Yirga <tkifle@gmail.com>
2020-06-28 20:45:42 +0300

    finished 6-8

--- projects/30-project-euler/script.js
+++ projects/30-project-euler/script.js
@@ -164,12 +164,132 @@ for (let index = 2; index <= 20; index++) {
 answer = Array.from(set).reduce((x, y) => x * y);
 
 
+// Problem 6
 
+// Sum square difference
+// The sum of the squares of the first ten natural numbers is,
+
+// 1^2+2^2+...+10^2=385
+// The square of the sum of the first ten natural numbers is,
+
+// (1+2+...+10)^2=55^2=3025
+// Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025−385=2640.
+
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+sum = 0;
+for (let index = 1; index <= 100; index++) {
+    sum += index;
+
+}
+let sqOfSum = sum * sum;
+sumOfSq = 0;
+for (let index = 1; index <= 100; index++) {
+    sumOfSq += index * index;
+
+}
+answer = sqOfSum - sumOfSq;
+
+
+function primeNumbers(amt) {
+    let primes = [2], isPrime, count = 1;
+    for (let i = 3; count < amt; i++) {
+        isPrime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+
+        }
+        if (isPrime) {
+            primes.push(i);
+            count++;
+        }
+
+    }
+    return primes;
+}
+
+// answer = primeNumbers(10001)[10000];
+
+// Problem 8
+
+// Largest product in a series
+// The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+
+// 73167176531330624919225119674426574742355349194934
+// 96983520312774506326239578318016984801869478851843
+// 85861560789112949495459501737958331952853208805511
+// 12540698747158523863050715693290963295227443043557
+// 66896648950445244523161731856403098711121722383113
+// 62229893423380308135336276614282806444486645238749
+// 30358907296290491560440772390713810515859307960866
+// 70172427121883998797908792274921901699720888093776
+// 65727333001053367881220235421809751254540594752243
+// 52584907711670556013604839586446706324415722155397
+// 53697817977846174064955149290862569321978468622482
+// 83972241375657056057490261407972968652414535100474
+// 82166370484403199890008895243450658541227588666881
+// 16427171479924442928230863465674813919123162824586
+// 17866458359124566529476545682848912883142607690042
+// 24219022671055626321111109370544217506941658960408
+// 07198403850962455444362981230987879927244284909188
+// 84580156166097919133875499200524063689912560717606
+// 05886116467109405077541002256983155200055935729725
+// 71636269561882670428252483600823257530420752963450
+
+// Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+
+let string = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
+
+(function () {
+    let current = [], highest = 0, product;
+    for (let index = 0; index < string.length; index++) {
+        if (index === 0) {
+            for (let j = 0; j < 13; j++) {
+                current.push(string[index + j]);
+            }
+            highest = current.reduce((x, y) => Number(x) * Number(y));
+        }
+        else {
+            current.shift();
+            current.push(string[index]);
+            product = current.reduce((x, y) => Number(x) * Number(y));
+            if (product > highest) {
+                highest = product;
+                console.log(current, highest);
+            }
+        }
+
+    }
+    return highest;
+});
+
+// Problem 9
+
+// Special Pythagorean triplet
+// A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+//     a^2 + b^2 = c^2
+// For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+
+
+
+// 1000 - a -b = 500 - ab/1000
+(function () {
+    let sum = 0;
+    for (let i = 1000; sum !== 1000; i++) {
+
+
+    }
+
+})();
 
 
 
 
 
 console.log(answer);
-
-
