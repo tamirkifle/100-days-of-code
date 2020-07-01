fdcf189a693049169bb8c60b2fcbe1d915e6b996
Tamir K. Yirga <tkifle@gmail.com>
2020-07-01 15:50:02 +0300

    Added line-through when complete

--- projects/31-to-do-list/js/script.js
+++ projects/31-to-do-list/js/script.js
@@ -6,12 +6,12 @@ function addItemToList(e) {
     if (addItemInput.value) {
         let toDo = document.createElement("div");
         toDo.classList.add("to-do");
-        let toDoInput = document.createElement("input");
-        toDoInput.classList.add("to-do-status");
-        toDoInput.id = "tag-1";
-        toDoInput.name = "tag-1";
-        toDoInput.type = "checkbox";
-        toDo.appendChild(toDoInput);
+        let toDoStatus = document.createElement("input");
+        toDoStatus.classList.add("to-do-status");
+        toDoStatus.id = "tag-1";
+        toDoStatus.name = "tag-1";
+        toDoStatus.type = "checkbox";
+        toDo.appendChild(toDoStatus);
         let toDoText = document.createElement("span");
         toDoText.classList.add("to-do-desc");
         toDoText.innerText = addItemInput.value;
@@ -20,6 +20,7 @@ function addItemToList(e) {
         toDoRemove.classList.add("remove");
         toDoRemove.innerText = "−";
         toDoRemove.addEventListener("click", removeParent);
+        toDoStatus.addEventListener("change", checkedToDo)
         toDo.appendChild(toDoRemove);
         document.getElementsByClassName("to-dos")[0].appendChild(toDo);
         addItemInput.value = "";
@@ -35,4 +36,15 @@ function removeParent(e) {
     e.target.parentElement.remove();
 
 
+}
+
+Array.from(document.getElementsByClassName("to-do-status")).forEach(element => element.addEventListener("change", checkedToDo));
+
+function checkedToDo(e) {
+    if (e.target.checked) {
+        e.target.parentElement.getElementsByClassName("to-do-desc")[0].style.textDecoration = "line-through";
+    }
+    else {
+        e.target.parentElement.getElementsByClassName("to-do-desc")[0].style.textDecoration = "none";
+    }
 }
