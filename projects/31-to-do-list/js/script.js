27b0af4c16791418149a89efa891e56be7857ce3
Tamir K. Yirga <tkifle@gmail.com>
2020-07-02 20:42:26 +0300

    mediaquery for desktop, array for todos

--- projects/31-to-do-list/js/script.js
+++ projects/31-to-do-list/js/script.js
@@ -1,29 +1,38 @@
-document.getElementsByClassName("add-item-btn")[0].addEventListener("click", addItemToList);
+document.getElementsByClassName("add-item-btn")[0].addEventListener("click", processToDoInput);
 let list = document.getElementsByClassName("to-dos")[0];
 
+let todos = [];
 
-function addItemToList(e) {
+let saved = ["Journal", "Code", "Exercise", "Meditate", "Read a Book"];
+saved.forEach(item => addToDo(item));
+
+function addToDo(item) {
+    todos.push(item);
+    let toDo = document.createElement("div");
+    toDo.classList.add("to-do");
+    let toDoStatus = document.createElement("input");
+    toDoStatus.classList.add("to-do-status");
+    toDoStatus.id = "tag-1";
+    toDoStatus.name = "tag-1";
+    toDoStatus.type = "checkbox";
+    toDo.appendChild(toDoStatus);
+    let toDoText = document.createElement("span");
+    toDoText.classList.add("to-do-desc");
+    toDoText.innerText = item;
+    toDo.appendChild(toDoText);
+    toDoRemove = document.createElement("span");
+    toDoRemove.classList.add("remove");
+    toDoRemove.innerText = "−";
+    toDoRemove.addEventListener("click", removeParent);
+    toDoStatus.addEventListener("change", checkedToDo)
+    toDo.appendChild(toDoRemove);
+    document.getElementsByClassName("to-dos")[0].appendChild(toDo);
+}
+
+function processToDoInput(e) {
     let addItemInput = document.getElementsByClassName("add-item-input")[0];
     if (addItemInput.value) {
-        let toDo = document.createElement("div");
-        toDo.classList.add("to-do");
-        let toDoStatus = document.createElement("input");
-        toDoStatus.classList.add("to-do-status");
-        toDoStatus.id = "tag-1";
-        toDoStatus.name = "tag-1";
-        toDoStatus.type = "checkbox";
-        toDo.appendChild(toDoStatus);
-        let toDoText = document.createElement("span");
-        toDoText.classList.add("to-do-desc");
-        toDoText.innerText = addItemInput.value;
-        toDo.appendChild(toDoText);
-        toDoRemove = document.createElement("span");
-        toDoRemove.classList.add("remove");
-        toDoRemove.innerText = "−";
-        toDoRemove.addEventListener("click", removeParent);
-        toDoStatus.addEventListener("change", checkedToDo)
-        toDo.appendChild(toDoRemove);
-        document.getElementsByClassName("to-dos")[0].appendChild(toDo);
+        addToDo(addItemInput.value);
         addItemInput.value = "";
         orderList();
 
@@ -46,9 +55,12 @@ Array.from(document.getElementsByClassName("to-do-status")).forEach(element => e
 function checkedToDo(e) {
     if (e.target.checked) {
         e.target.parentElement.getElementsByClassName("to-do-desc")[0].style.textDecoration = "line-through";
+        e.target.parentElement.style.backgroundColor = "rgb(239, 255, 234)";
     }
     else {
         e.target.parentElement.getElementsByClassName("to-do-desc")[0].style.textDecoration = "none";
+        e.target.parentElement.style.backgroundColor = "floralwhite";
+
     }
     orderList();
 }
