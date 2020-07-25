f92e42ad5be71a97a51400ceb1d4af4a92b96f17
Tamir K. Yirga <tkifle@gmail.com>
2020-07-25 18:04:02 +0300

    did skipbtn, progress bar, and progress scrub

--- projects/40-js30-custom-video-player/scripts.js
+++ projects/40-js30-custom-video-player/scripts.js
@@ -11,3 +11,23 @@ playBtn.addEventListener("click", () => video.paused ? video.play() : video.paus
 
 const scrubbers = document.querySelectorAll(".player__slider");
 scrubbers.forEach(scrubber => scrubber.addEventListener("change", (e) => video[e.target.name] = e.target.value));
+
+
+const skipBtns = document.querySelectorAll("[data-skip]");
+skipBtns.forEach(skipBtn => skipBtn.addEventListener("click", (e) => video.currentTime += Number(e.target.getAttribute("data-skip"))));
+
+const progressBar = document.querySelector(".progress__filled");
+video.addEventListener("timeupdate", () => moveProgressBar(video.currentTime));
+
+function moveProgressBar(time) {
+    console.log("changed");
+    progressBar.style.flexBasis = `${time / video.duration * 100}%`;
+}
+
+const progressContainer = document.querySelector(".progress");
+progressContainer.addEventListener("click", scrub);
+
+function scrub(e) {
+    video.currentTime = e.layerX / progressContainer.offsetWidth * video.duration;
+
+}
