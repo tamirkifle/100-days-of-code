2890de39f911758e680f1ea974f7d3d680d4fa45
Tamir K. Yirga <tkifle@gmail.com>
2020-07-25 17:06:08 +0300

    did the volume and playbackRate scrubbers

--- projects/40-js30-custom-video-player/scripts.js
+++ projects/40-js30-custom-video-player/scripts.js
@@ -6,8 +6,8 @@ video.addEventListener("play", () => playBtn.innerText = "❚❚");
 video.addEventListener("pause", () => playBtn.innerText = "►");
 
 const playBtn = document.querySelector(".player__button");
-
-
-
 playBtn.addEventListener("click", () => video.paused ? video.play() : video.pause());
 
+
+const scrubbers = document.querySelectorAll(".player__slider");
+scrubbers.forEach(scrubber => scrubber.addEventListener("change", (e) => video[e.target.name] = e.target.value));
