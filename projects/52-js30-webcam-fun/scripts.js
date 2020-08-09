27f34bbffe1dcd0c04b5540dd7ee1815e2e9b263
Tamir K. Yirga <tkifle@gmail.com>
2020-08-09 17:41:26 +0300

    Added some effects

--- projects/52-js30-webcam-fun/scripts.js
+++ projects/52-js30-webcam-fun/scripts.js
@@ -13,8 +13,6 @@ function getVideo() {
 }
 
 
-getVideo()
-    .then(videoToCanvas);
 
 function videoToCanvas() {
     canvas.width = video.videoWidth;
@@ -22,19 +20,81 @@ function videoToCanvas() {
 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        let pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        // pixels = redEffect(pixels);
+        // pixels = hulkEffect(pixels);
+        // pixels = rgbSplit(pixels);
+        // pixels = nightMode(pixels);
+        // ctx.globalAlpha = 0.1;
+
+        pixels = greenScreen(pixels);
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
 
 function takePhoto() {
+    let photoURI = canvas.toDataURL('image/jpeg');
     snap.currentTime = 0;
     snap.play();
-    let photoURI = canvas.toDataURL('image/jpeg');
     let link = document.createElement("a");
     link.innerHTML = `<img src=${photoURI} alt="Snapshot"></img>`;
     // link.innerText = "Photo";
     link.setAttribute("download", "IMG_SNAP");
     link.href = photoURI;
     strip.insertBefore(link, strip.firstChild);
-
 }
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i] += 100; //r
+        pixels.data[i + 1] -= 100; //g
+        pixels.data[i + 2] *= 0.5; //b
+    }
+    return pixels;
+}
+
+function hulkEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 30]; //r
+        pixels.data[i + 1] = pixels.data[i + 40]; //g
+        pixels.data[i + 2] = pixels.data[i + 50]; //b
+    }
+    return pixels;
+}
+
+function nightMode(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 100]; //r
+        pixels.data[i + 1] = pixels.data[i + 100]; //g
+        pixels.data[i + 2] = pixels.data[i + 100]; //b
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; //r
+        pixels.data[i + 100] = pixels.data[i + 1]; //g
+        pixels.data[i - 150] = pixels.data[i + 2]; //b
+    }
+    return pixels;
+}
+
+
+function greenScreen(pixels) {
+    let levels = {};
+    document.querySelectorAll(".rgb input").forEach(input => levels[input.name] = input.value);
+    console.log(pixels.data);
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        if (pixels.data[i + 0] >= levels.rmin && pixels.data[i + 0] <= levels.rmax
+            && pixels.data[i + 1] >= levels.gmin && pixels.data[i + 1] <= levels.gmax
+            && pixels.data[i + 2] >= levels.bmin && pixels.data[i + 2] <= levels.bmax) {
+            pixels.data[i + 3] = 0;
+        }
+    }
+
+    return pixels;
+}
+getVideo()
+    .then(videoToCanvas);
