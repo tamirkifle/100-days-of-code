f39c903672e2ba89c85886a47d683ac404109a41
Tamir K. Yirga <tkifle@gmail.com>
2020-08-13 11:00:30 +0300

    added active states, started fultionality

---
+++ projects/55-fem-static-job-listings/script.js
@@ -0,0 +1,8 @@
+
+const filtersContainer = document.createElement("div");
+filtersContainer.classList.add("filters-container");
+const filterTablet = document.createElement("div");
+filterTablet.classList.add("filter-tablet");
+
+
+
